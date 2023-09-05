e0da0504f2043a5b0bf32797a7c28591a1b40977
Preeti Kurmi <kurmipreeti54@gmail.com>
2023-09-05 09:30:16 +0530

    change backgroung color and font as bold

--- app.js
+++ app.js
@@ -14,3 +14,8 @@ const addItemsHeading = document.querySelector('.title');
 // Apply styles to make it bold and green
 addItemsHeading.style.fontWeight = 'bold';
 addItemsHeading.style.color = 'green';
+ var items=document.getElementsByClassName("list-group-item");
+ items[2].style.backgroundColor="green";
+ for(var i=0;i<items.length;i++){
+    items[i].style.fontWeight="bold";
+ }
