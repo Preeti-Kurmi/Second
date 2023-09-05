544ede7d8835aadb1694836e4ef2696a278d8c34
Preeti Kurmi <kurmipreeti54@gmail.com>
2023-09-05 09:12:42 +0530

    Adding html and js file

--- app.js
+++ app.js
@@ -1,2 +1,16 @@
-console.log("I am in github")
-console.log("i am in second line");
+// console.dir(document);
+// console.log(document.URL);
+// console.log(document.title);
+// console.log(document.head);
+// console.log(document.body);
+// console.log(document.doctype);
+// console.log(document.forms);
+// console.log(document.images);
+// console.log(document.links);
+let a =document.getElementById('main-header');
+a.style.border="5px solid black";
+const addItemsHeading = document.querySelector('.title');
+
+// Apply styles to make it bold and green
+addItemsHeading.style.fontWeight = 'bold';
+addItemsHeading.style.color = 'green';
