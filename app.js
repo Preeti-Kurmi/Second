f555d45cd8ca2f681093b193657134407b94fe06
Preeti Kurmi <kurmipreeti54@gmail.com>
2023-09-06 17:00:58 +0530

    Adding functionality of add and dlt

--- app.js
+++ app.js
@@ -53,64 +53,131 @@
 // item.parentNode.style.backgroundColor="pink";
 // console.log(item.parentNode.parentNode.parentNode);
 //ParentElement
-var item=document.querySelector("#items");
-console.log(item.parentElement);
-item.parentNode.style.backgroundColor="yellow";
-console.log(item.parentElement.parentElement.parentElement);
+// var item=document.querySelector("#items");
+// console.log(item.parentElement);
+// item.parentNode.style.backgroundColor="yellow";
+// console.log(item.parentElement.parentElement.parentElement);
 //Firstchild
-console.log(item.firstChild);
+//console.log(item.firstChild);
 //FirstChildElement
-console.log(item.firstElementChild);
-item.firstElementChild.style.backgroundColor="pink";
-item.firstElementChild.textContent="HEllo World";
+//console.log(item.firstElementChild);
+//item.firstElementChild.style.backgroundColor="pink";
+//item.firstElementChild.textContent="HEllo World";
 //lastchild
-console.log(item.lasChild);
+//console.log(item.lasChild);
 //FirstChildElement
-console.log(item.lastElementChild);
-item.lastElementChild.style.backgroundColor="pink";
-item.lastElementChild.textContent="HEllo World";
+//console.log(item.lastElementChild);
+//item.lastElementChild.style.backgroundColor="pink";
+//item.lastElementChild.textContent="HEllo World";
 //nextsibling
-console.log(item.nextSibling);
-console.log(item.nextElementSibling);
+//console.log(item.nextSibling);
+//console.log(item.nextElementSibling);
 //previous sibling
-console.log(item.previousSibling);
+//console.log(item.previousSibling);
 //previous sibling Element
-console.log(item.previousElementSibling);
-item.previousElementSibling.style.color="red";
+//console.log(item.previousElementSibling);
+//item.previousElementSibling.style.color="red";
 //new div
-var a = document.createElement('div');
-console.log(a);
-a.className="hello";
-a.id="h";
-a.setAttribute('title','heelo world');
-console.log(a);
-var b=document.createTextNode("Hello word");
-a.append(b);
-console.log(a);
-var container=document.querySelector("header .container");
-var h1=document.querySelector("header h1");
-console.log(a);
-container.insertBefore(a,h1);
-a.style.fontSize="40px";
-// Get the <ul> element with id "items"
-const itemsList = document.getElementById('items');
+//v//ar a = document.createElement('div');
+//console.log(a);
+// a.className="hello";
+// a.id="h";
+// a.setAttribute('title','heelo world');
+// console.log(a);
+// var b=document.createTextNode("Hello word");
+// a.append(b);
+// console.log(a);
+// var container=document.querySelector("header .container");
+// var h1=document.querySelector("header h1");
+// console.log(a);
+// container.insertBefore(a,h1);
+// a.style.fontSize="40px";
+// // Get the <ul> element with id "items"
+// const itemsList = document.getElementById('items');
 
 // Add event listeners to delete buttons
-itemsList.addEventListener('click', function(e) {
-  if (e.target.classList.contains('delete-item')) {
+//itemsList.addEventListener('click', function(e) {
+ // if (e.target.classList.contains('delete-item')) {
     // Remove the parent <li> of the delete button
-    e.target.parentElement.remove();
-  }
-});
+   // e.target.parentElement.remove();
+  //}
+//});
 
 // Add edit button next to the delete icon for each <li>
-const listItems = document.querySelectorAll('li');
-listItems.forEach(function(item) {
+//const listItems = document.querySelectorAll('li');
+//listItems.forEach(function(item) {
   // Create an edit button element
-  const editButton = document.createElement('button');
-  editButton.className = 'btn btn-primary btn-sm ml-2'; // Bootstrap classes
-  editButton.textContent = 'Edit'; // Button text
+  //const editButton = document.createElement('button');
+  //editButton.className = 'btn btn-primary btn-sm ml-2'; // Bootstrap classes
+  //editButton.textContent = 'Edit'; // Button text
 
   // Append the edit button to the <li>
-  item.appendChild(editButton);
-});
+  //item.appendChild(editButton);
+//});
+var form = document.getElementById('addForm');
+var itemList = document.getElementById('items');
+var filter = document.getElementById('filter');
+
+// Form submit event
+form.addEventListener('submit', addItem);
+// Delete event
+itemList.addEventListener('click', removeItem);
+// Filter event
+filter.addEventListener('keyup', filterItems);
+
+// Add item
+function addItem(e){
+  e.preventDefault();
+
+  // Get input value
+  var newItem = document.getElementById('item').value;
+
+  // Create new li element
+  var li = document.createElement('li');
+  // Add class
+  li.className = 'list-group-item';
+  // Add text node with input value
+  li.appendChild(document.createTextNode(newItem));
+
+  // Create del button element
+  var deleteBtn = document.createElement('button');
+
+  // Add classes to del button
+  deleteBtn.className = 'btn btn-danger btn-sm float-right delete';
+
+  // Append text node
+  deleteBtn.appendChild(document.createTextNode('X'));
+
+  // Append button to li
+  li.appendChild(deleteBtn);
+
+  // Append li to list
+  itemList.appendChild(li);
+}
+
+// Remove item
+function removeItem(e){
+  if(e.target.classList.contains('delete')){
+    if(confirm('Are You Sure?')){
+      var li = e.target.parentElement;
+      itemList.removeChild(li);
+    }
+  }
+}
+
+// Filter Items
+// function filterItems(e){
+//   // convert text to lowercase
+//   var text = e.target.value.toLowerCase();
+//   // Get lis
+//   var items = itemList.getElementsByTagName('li');
+//   // Convert to an array
+//   Array.from(items).forEach(function(item){
+//     var itemName = item.firstChild.textContent;
+//     if(itemName.toLowerCase().indexOf(text) != -1){
+//       item.style.display = 'block';
+//     } else {
+//       item.style.display = 'none';
+//     }
+//   });
+// }
